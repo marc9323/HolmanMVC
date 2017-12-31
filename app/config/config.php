b1d7ab68f739bf41cd395ec10ace916a51e9f3e3
marc9323 <marcholman9323@gmail.com>
2017-12-30 19:03:15 -0600

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// replace this test data with your own environmental variables
+
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'vapethis');
+define('DB_NAME', 'tmvc');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost:42000/holmanmvc');
+
+// Site name
+define('SITENAME', 'HolmanMVC');
